ce6db6ea362bd881dfc9e86883a7c297f260862e
Andre Silva <andreswebs@pm.me>
2024-04-11 17:52:41 -0300

    tests

--- tests/main.tf
+++ tests/main.tf
@@ -1,10 +1,3 @@
 module "apigw_sqs" {
-  source                                    = "../"
-  api_key_enabled                           = false
-  lambda_authorizer_enabled                 = true
-  lambda_authorizer_openapi_security_scheme = <<-EOT
-    {
-      "test": "ok"
-    }
-  EOT
+  source = "../"
 }
